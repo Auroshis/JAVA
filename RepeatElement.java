8178f659cb2bd14af6e7d7ff0a6b313250206d15
Auroshis <rayauroshis@gmail.com>
2020-03-07 21:02:53 +0530

    added 3 varieties of finding duplicate elements in array

--- RepeatElement.java
+++ RepeatElement.java
@@ -1,9 +1,73 @@
+/*Applicable for positive elements only
+You have to take size of count array = biggest element */
+
+/*Basic Method
 class RepeatElement 
 { 
 	void printRepeating(int arr[], int size) 
 	{ 
-		int count[] = new int[size]; 
+		int i, j; 
+		System.out.println("Repeated Elements are :"); 
+		for (i = 0; i < size; i++) 
+		{ 
+			for (j = i + 1; j < size; j++) 
+			{ 
+				if (arr[i] == arr[j]) 
+					System.out.print(arr[i] + " "); 
+			} 
+		} 
+	} 
+
+	public static void main(String[] args) 
+	{ 
+		RepeatElement repeat = new RepeatElement(); 
+		int arr[] = {4, 2, 4, 5, 2, 3, 1}; 
+		int arr_size = arr.length; 
+		repeat.printRepeating(arr, arr_size); 
+	} 
+} 
+Time-O(n*n) Space - O(1)
+*/ 
+
+/*
+class RepeatElement 
+{ 
+	void printRepeating(int arr[], int size) 
+	{ 
 		int i; 
+		System.out.println("The repeating elements are : "); 
+	
+		for(i = 0; i < size; i++) 
+		{ 
+			if(arr[Math.abs(arr[i])] > 0) 
+				arr[Math.abs(arr[i])] = -arr[Math.abs(arr[i])]; 
+			else
+				System.out.print(Math.abs(arr[i]) + " "); 
+		}		 
+	} 
+
+	
+	public static void main(String[] args) 
+	{ 
+		RepeatElement repeat = new RepeatElement(); 
+		int arr[] = {4, 2, 4, 5, 2, 3, 1}; 
+		int arr_size = arr.length; 
+		repeat.printRepeating(arr, arr_size); 
+	} 
+} 
+
+Time - O(n) Space - O(1)
+*/
+
+
+
+import java.util.*;
+class RepeatElement 
+{   
+	void printRepeating(int arr[], int size, int k) 
+	{ 
+		int count[] = new int[size]; 
+		int i,kth_count=0; 
 
 		System.out.println("Repeated elements are : "); 
 		for (i = 0; i < size; i++) 
@@ -18,17 +82,32 @@ class RepeatElement
 			else
 				count[arr[i]]++; 
 		} 
+		for(i=0; i < size; i++){
+			 if(count[i] >= 1){
+				 kth_count++;
+			 }
+			 if(k==kth_count)
+			 {
+				 System.out.println("The"+k+" repeated element is"+arr[i]);
+				 break;
+			 }
+		}
 		System.out.println("\nCount array");
 		for(i = 0; i<size;i++){
 			System.out.print(count[arr[i]]);
 		}
 	} 
+	 
 
-	public static void main(String[] args) 
+ 	public static void main(String[] args) 
 	{ 
 		RepeatElement repeat = new RepeatElement(); 
+		System.out.println("enter the k to find kth repeated element");
+		Scanner sc = new Scanner(System.in);
+		int k = sc.nextInt();
 		int arr[] = {4,2,4,2,2}; 
 		int arr_size = arr.length; 
-		repeat.printRepeating(arr, arr_size); 
+		repeat.printRepeating(arr, arr_size, k); 
 	} 
 } 
+//Time - O(n) Space - O(n)
